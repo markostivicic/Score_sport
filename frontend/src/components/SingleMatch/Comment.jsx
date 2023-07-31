99bed2441871aec3013853d90a97913cfb9aa31b
Kristijan Paradzikovic <kikiparadz@gmail.com>
2023-07-31 15:00:31 +0200

    Add modal

--- frontend/src/components/SingleMatch/Comment.jsx
+++ frontend/src/components/SingleMatch/Comment.jsx
@@ -2,23 +2,28 @@ import React from "react";
 import { useResultContext } from "../../context/ResultContext";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import {
+  extractHoursAndMinutes,
+  extractDate,
+} from "../../services/DateTimeService";
 
-export default function Comment({ comment }) {
+export default function Comment({ comment, setSelectedCommentId }) {
   const { authenticatedUser } = useResultContext();
-
-  function handleOnClickDelete() {}
 
   return (
     <div className="d-flex align-items-center gap-3">
       <div className="max-width">
         <strong>{comment.user.userName}</strong>
+        <span className="mx-3">
+          {extractDate(comment.time)} {extractHoursAndMinutes(comment.time)}
+        </span>
         <p className="white-space-initial">{comment.text}</p>
       </div>
       {authenticatedUser?.username === comment.user.userName && (
         <FontAwesomeIcon
           className="text-danger min-width-10 cursor-pointer"
           icon={faTrash}
-          onClick={handleOnClickDelete}
+          onClick={() => setSelectedCommentId(comment.id)}
         />
       )}
     </div>
--- frontend/src/components/SingleMatch/CommentSection.jsx
+++ frontend/src/components/SingleMatch/CommentSection.jsx
@@ -3,14 +3,18 @@ import Button from "../Button";
 import Form from "../Form";
 import {
   createNewCommentAsync,
+  deleteCommentByIdAsync,
   getCommentsAsync,
 } from "../../services/CommentService";
 import { useNavigate } from "react-router-dom";
 import Comment from "./Comment";
+import Modal from "../Modal";
 
 export default function CommentSection({ matchId }) {
   const [isFormActive, setIsFormActive] = useState(false);
   const [comments, setComments] = useState([]);
+
+  const [selectedCommentId, setSelectedCommentId] = useState(null);
 
   const navigate = useNavigate();
 
@@ -38,27 +42,52 @@ export default function CommentSection({ matchId }) {
     setIsFormActive(false);
     form.elements["comment-area"].value = "";
   }
+
+  const handleConfirmDelete = () => {
+    deleteCommentAsync(selectedCommentId);
+    setSelectedCommentId(null);
+  };
+
+  async function deleteCommentAsync(id) {
+    await deleteCommentByIdAsync(id, navigate);
+    getAllCommentsAsync();
+  }
+
   return (
-    <div className="align-self-center mt-5 d-flex flex-column width-400 max-height-full">
-      <Button
-        handleOnClick={() => setIsFormActive(true)}
-        text="Komentiraj"
-        color="secondary"
+    <>
+      <div className="align-self-center mt-5 d-flex flex-column width-400 max-height-full">
+        <Button
+          handleOnClick={() => setIsFormActive(true)}
+          text="Komentiraj"
+          color="secondary"
+        />
+        <div className={`${!isFormActive && "d-none"}`}>
+          <Form handleOnSubmit={handleOnSubmit} buttonText="Dodaj komentar">
+            <textarea
+              id="comment-area"
+              required
+              className="my-3 resize-none"
+            ></textarea>
+          </Form>
+        </div>
+        <div className="mt-4 d-flex flex-column overflow-y-auto">
+          {comments.map((comment) => {
+            return (
+              <Comment
+                key={comment.id}
+                comment={comment}
+                setSelectedCommentId={setSelectedCommentId}
+              />
+            );
+          })}
+        </div>
+      </div>
+      <Modal
+        selectedItem={selectedCommentId}
+        handleCancelDelete={() => setSelectedCommentId(null)}
+        handleConfirmDelete={handleConfirmDelete}
+        isComment={true}
       />
-      <div className={`${!isFormActive && "d-none"}`}>
-        <Form handleOnSubmit={handleOnSubmit} buttonText="Dodaj komentar">
-          <textarea
-            id="comment-area"
-            required
-            className="my-3 resize-none"
-          ></textarea>
-        </Form>
-      </div>
-      <div className="mt-4 d-flex flex-column overflow-y-auto">
-        {comments.map((comment) => {
-          return <Comment key={comment.id} comment={comment} />;
-        })}
-      </div>
-    </div>
+    </>
   );
 }
